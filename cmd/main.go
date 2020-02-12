be3c891ab37975436d5cec0ba9cb4c315f2b4341
Avi Deitcher <avi@deitcher.net>
2020-02-12 10:35:59 +0200

    common processing for all packet sources

--- cmd/main.go
+++ cmd/main.go
@@ -29,6 +29,7 @@ var rootCmd = &cobra.Command{
 			err    error
 			c      chan pcap.Packet
 			handle *pcap.Handle
+			count  int
 		)
 		if len(args) >= 1 {
 			iface = args[0]
@@ -43,32 +44,17 @@ var rootCmd = &cobra.Command{
 				log.Fatal(err)
 			}
 			packetSource := gopacket.NewPacketSource(handle, layers.LinkTypeEthernet)
-			var count int
 			for packet := range packetSource.Packets() {
-				if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-					fmt.Printf("%d: TCP packet ", count)
-					// Get actual TCP data from this layer
-					tcp, _ := tcpLayer.(*layers.TCP)
-					fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
-				}
-				// Iterate over all layers, printing out each layer type
-				for i, layer := range packet.Layers() {
-					fmt.Printf("%d: PACKET LAYER %d: %s\n", count, i, layer.LayerType())
-				}
-
-				data := packet.Data()
-				if len(data) > 50 {
-					data = data[:50]
-				}
-				fmt.Printf("%d: packet size %d, first bytes %d\n", count, packet.Metadata().CaptureLength, data)
+				processPacket(packet, count)
 				count++
 			}
 		} else {
 			if c, err = pcap.Listen(iface, 65536, true, true, 0); err != nil {
 				log.Fatal(err)
 			}
-			for p := range c {
-				fmt.Printf("packet size %d, first bytes %#v\n", p.Info.CaptureLength, p.B[:50])
+			for packet := range c {
+				processPacket(gopacket.NewPacket(packet.B, layers.LayerTypeEthernet, gopacket.Default), count)
+				count++
 			}
 		}
 	},
@@ -77,4 +63,23 @@ var rootCmd = &cobra.Command{
 func init() {
 	rootCmd.Flags().BoolVar(&useGopacket, "gopacket", false, "use gopacket interface instead of simple pcap.Listen")
 	rootCmd.Flags().BoolVar(&debug, "debug", false, "print lots of debugging messages")
+}
+
+func processPacket(packet gopacket.Packet, count int) {
+	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+		fmt.Printf("%d: TCP packet ", count)
+		// Get actual TCP data from this layer
+		tcp, _ := tcpLayer.(*layers.TCP)
+		fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
+	}
+	// Iterate over all layers, printing out each layer type
+	for i, layer := range packet.Layers() {
+		fmt.Printf("%d: PACKET LAYER %d: %s\n", count, i, layer.LayerType())
+	}
+
+	data := packet.Data()
+	if len(data) > 50 {
+		data = data[:50]
+	}
+	fmt.Printf("%d: packet size %d, first bytes %d\n", count, packet.Metadata().CaptureLength, data)
 }
